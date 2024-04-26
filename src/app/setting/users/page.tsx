eed3bf9a30aca7289bbcc24ce9b06b4322f0f648
Randy Rebucas <rebucasrandy1986@gmail.com>
2024-04-26 11:00:06 +0800

    update

--- src/app/setting/users/page.tsx
+++ src/app/setting/users/page.tsx
@@ -1,6 +1,9 @@
 import { fetchUsers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
 import Table from '@/app/ui/setting/users/table';
+import { PlusIcon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: 'Users',
@@ -8,6 +11,22 @@ export const metadata: Metadata = {
 
 export default async function Page() {
     const users = await fetchUsers();
-    
-    return <Table users={users}/>;
+
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Users</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                {/* <Search placeholder="Search invoices..." /> */}
+                <Link
+                    href="/setting/users/create"
+                    className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                >
+                    <span className=" md:block">Create User</span>
+                </Link>
+            </div>
+            <Table users={users} />
+        </div>
+    )
 }                                                          
