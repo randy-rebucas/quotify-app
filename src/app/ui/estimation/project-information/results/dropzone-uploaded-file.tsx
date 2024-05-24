7f566d0b3f66571664f9bd756249c4348fd6fe54
Randy Rebucas <rebucasrandy1986@gmail.com>
2024-05-24 17:10:28 +0800

    updated upload result

--- src/app/ui/estimation/project-information/results/dropzone-uploaded-file.tsx
+++ src/app/ui/estimation/project-information/results/dropzone-uploaded-file.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { PlanData } from "../steps/plan";
+import { filesize } from "filesize";
 
 export default function DropzoneUploadedFile({ floorPlans, updateFields }: { floorPlans: any[], updateFields: (fields: Partial<PlanData>) => void }) {
 
@@ -7,23 +8,36 @@ export default function DropzoneUploadedFile({ floorPlans, updateFields }: { flo
     updateFields({ floorPlans: floorPlans.filter((file: any, id: number) => id !== idx) })
   }
 
+  const isImageType = ['image/jpeg', 'image/jpg', 'image/png'];
   return (
     <>
       {floorPlans.length && <div className="mt-[30.833vh] px-30 w-full">
         <div className="dropzone-uploads">
           <p className="text-darkblue font-latobold">my floorplans</p>
-          <div className="dropzone-content mt-10 flex flex-wrap">
-            {/* <!-- Uploaded files will automatically be shown here --> */}
+          <div className="flex flex-col gap-3 mt-10">
             {floorPlans.map((file: any, idx: any) => (
-              <div key={idx} className="flex flex-row space-x-5">
-                <span>{file.name}</span>
-                <span
-                  className="text-red-500 cursor-pointer"
-                  onClick={() => removeFile(idx)}
-                >
+              <div key={idx} className="flex gap-3 relative">
+                {isImageType.includes(file.type) && <Image src={URL.createObjectURL(file)} alt={""} width={100} height={100} />}
+                {!isImageType.includes(file.type) && <div className="icon-upload"></div>}
+                <div>
+                  <p>{file.name}</p>
+                  <p>{file.type}</p>
+                  <p>{filesize(file.size, { standard: "jedec" })}</p>
+                </div>
+                <span className="absolute cursor-pointer right-0 text-red-500"
+                  onClick={() => removeFile(idx)} >
                   remove
                 </span>
               </div>
+              // <div key={idx} className="flex flex-row space-x-5">
+              //   <span>{file.name}</span>
+              //   <span
+              //     className="text-red-500 cursor-pointer"
+              //     onClick={() => removeFile(idx)}
+              //   >
+              //     remove
+              //   </span>
+              // </div>
             ))}
           </div>
         </div>
