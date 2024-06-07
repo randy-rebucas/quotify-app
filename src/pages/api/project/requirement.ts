c8cdc139b8cfa7ec390d12df9c4198abfe10fa78
Randy Rebucas <rebucasrandy1986@gmail.com>
2024-06-07 16:24:24 +0800

    update

--- src/pages/api/project/requirement.ts
+++ src/pages/api/project/requirement.ts
@@ -1,5 +1,6 @@
 import Estimate from "@/app/models/Estimate";
 import EstimateRequirement from "@/app/models/EstimateRequirement";
+import Project from "@/app/models/Project";
 import { StimateData } from "@/app/ui/estimation/requirement/entities";
 import connect from "@/app/utils/db";
 
@@ -9,13 +10,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
   connect();
 
   const { stimates, projectId, section } = req.body;
 
   try {
-
     stimates.map(async (stimate: StimateData) => {
       const estimate = new Estimate({
         name: stimate.name,
@@ -29,15 +28,22 @@ export default async function handler(
         estimate: estimateId,
         requirements: {
           finish: stimate.requirement.finish?.value,
-          sustainabilityCertification: stimate.requirement.sustainabilityCertification?.value,
+          sustainabilityCertification:
+            stimate.requirement.sustainabilityCertification?.value,
           mepFeatures: stimate.requirement.mepFeatures?.value,
           buildingCondition: stimate.requirement.buildingCondition?.value,
           technology: stimate.requirement.technology?.value,
-          furniture: stimate.requirement.furniture?.value
-        }
+          furniture: stimate.requirement.furniture?.value,
+        },
       });
       await requirement.save();
     });
+
+    const update = { lastUri: "refinement" };
+
+    const filterProject = { _id: projectId };
+
+    await Project.findOneAndUpdate(filterProject, update);
 
     res.status(200).json({ id: projectId });
   } catch (err) {
