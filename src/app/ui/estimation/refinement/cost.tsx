54d078d04276402233917df522d6b04d9bf135e9
Randy <rebucasrandy1986@gmail.com>
2024-08-16 10:26:20 +0800

    update

--- src/app/ui/estimation/refinement/cost.tsx
+++ src/app/ui/estimation/refinement/cost.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import { useRequirementLevelStore } from "@/app/lib/store/requirementLevelStore";
 
 export default function Cost() {
-    const requirementLevelUnitRate = useRequirementLevelStore(
-        (state) => state.requirementLevelUnitRate
-    );
 
     return (
         <div className="bg-darkgreen2 p-30 flex items-center sticky w-full bottom-0 justify-between text-white">
@@ -14,7 +10,7 @@ export default function Cost() {
                 per square foot
             </span>
             <span className="text-[53px] font-latoblack">
-                ${requirementLevelUnitRate ?? 0}
+                $0
             </span>
         </div>
     )
