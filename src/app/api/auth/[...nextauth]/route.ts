12d19243168a897e11d95889946fff5e6695341c
Randy <rebucasrandy1986@gmail.com>
2024-09-04 10:09:11 +0800

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,67 +1,6 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { z } from "zod";
-import bcrypt from "bcrypt";
-import connect from "@/app/utils/db";
-import User from "@/app/models/User";
+import { authOptions } from "../../../../../authOptions";
 
-async function getUser(email: string) {
-  try {
-    connect();
-
-    const user = await User.findOne({ email })
-      .populate("office")
-      .populate("auth")
-      .exec();
-
-    const transformedData = {
-      id: user._id.toString(),
-      email: user.email,
-      name: user.name,
-      password: user.auth.password,
-      roles: user.roles,
-    };
-
-    return transformedData;
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
-  }
-}
-
-const handler = NextAuth({
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/login",
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials) {
-
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-
-          if (passwordsMatch) return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
-});
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST };
