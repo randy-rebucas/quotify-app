690a10da0ca31c67de6e955bef5303bd0607d194
randy <rebucasrandy1986@gmail.com>
2024-09-13 21:47:45 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,32 @@
-import '@/app/global.css';
+import "@/app/global.css";
 import "../../public/css/main.css";
 
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
-import { Lato } from 'next/font/google'
-import SessionProvider from '../components/session-provider';
-import { getSession } from '@/actions/session';
-import { Toaster } from 'react-hot-toast';
+import { Lato } from "next/font/google";
+import SessionProvider from "../components/session-provider";
+import { getSession } from "@/actions/session";
+import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const lato = Lato({
   weight: ["100", "300", "400", "700", "900"],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: {
-    template: '%s',
-    default: 'Quotify',
+    template: "%s",
+    default: "Quotify",
   },
-  description: 'Quotify.'
+  description: "Quotify.",
 };
 
 export default async function RootLayout({
-  children, modal
+  children,
+  modal,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
@@ -33,6 +35,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <link href="static/styles/base.css" rel="stylesheet" />
+      </Head>
       <body className={`${lato.className} antialiased`}>
         <Toaster position="top-center" />
         <SessionProvider session={session}>
