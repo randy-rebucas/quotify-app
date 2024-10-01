8d4981478ee94f0000d0dcbf8321e0a7ade09701
Randy <rebucasrandy1986@gmail.com>
2024-10-01 20:03:40 +0800

    update api

--- src/pages/api/project/amenities/by-project/[pid].ts
+++ src/pages/api/project/amenities/by-project/[pid].ts
@@ -9,24 +9,28 @@ export default async function handler(
   res: NextApiResponse
 ) {
   connect();
+  if (req.method === "GET") {
+    const { pid } = req.query;
 
-  const { pid } = req.query;
+    try {
+      const project_amenities = await ProjectAmenity.find({ project: pid })
+        .populate("amenity")
+        .exec();
 
-  try {
-    const project_amenities = await ProjectAmenity.find({ project: pid })
-      .populate("amenity")
-      .exec();
+      const transformData = project_amenities.map((project_amenity) => {
+        return {
+          _id: project_amenity._id.toString(), // project amenity id
+          name: project_amenity.amenity.amenityName, // amenity name
+          type: "amenity",
+        };
+      });
 
-    const transformData = project_amenities.map((project_amenity) => {
-      return {
-        _id: project_amenity._id.toString(),                                            // project amenity id
-        name: project_amenity.amenity.amenityName,                               // amenity name
-        type: 'amenity'
-      };
-    });
-
-    res.status(200).json(transformData);
-  } catch (err) {
-    res.status(500).json(err);
+      res.status(200).json(transformData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
